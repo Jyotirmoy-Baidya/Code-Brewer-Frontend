3798fc54b7decc1fb3aefd2775b95e50f17ab5b8
ghosh-trisha <trisha.ghosh1620@gmail.com>
2024-08-19 13:09:27 +0530

    commited

--- src/pages/Contests.jsx
+++ src/pages/Contests.jsx
@@ -53,7 +53,7 @@ const Contests = () => {
                 <div className="p-2 grow grid grid-cols-2 gap-10 overflow-scroll design-scrollbar contest-list">
                     {
                         loading === true ? <div className='flex text-xl gap-4 items-center'>Fetching Contests<AiOutlineLoading3Quarters className='text-lg loading-spin' /></div> :
-                            !contests.length > 0 ?
+                            !contests?.length > 0 ?
                                 <div className='flex text-xl gap-4 items-center'>
                                     Due to server issue unable to fetch the contests.
                                 </div> :
